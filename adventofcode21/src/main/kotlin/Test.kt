674ee706ff761c78e976e2835d8782c590383fe9
Alexander Kröker <alexander.kroeker@iosb.fraunhofer.de>
2021-12-16 11:23:01 +0100

    update

--- adventofcode21/src/main/kotlin/Test.kt
+++ adventofcode21/src/main/kotlin/Test.kt
@@ -1,11 +1,14 @@
 import java.io.File
-import java.sql.Timestamp
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.time.Duration
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-fun mapCSV(fileName: String): MutableMap<Long, Double> {
-    val map = mutableMapOf<Long, Double>()
+fun mapCSV(fileName: String): MutableMap<String, Double> {
+    val map = mutableMapOf<String, Double>()
 
     val formatter = DateTimeFormatter.ofPattern(" dd MMM yyyy HH:mm", Locale.GERMAN)
     val formatter2 = DateTimeFormatter.ofPattern(" d MMM yyyy HH:mm", Locale.GERMAN)
@@ -13,23 +16,20 @@ fun mapCSV(fileName: String): MutableMap<Long, Double> {
     File(fileName).forEachLine { line ->
         val split = line.split(",")
         if (split.size == 2) {
-            dateStr = split[0].replace("\"", "")
+            dateStr = split[0].replace("\"", "").replace("Mrz", "Mär")
         } else if (split.size > 2) {
             if (dateStr != "") {
                 val timeStr = split[0].replace("\"", "")
-
                 val weight = split[1].replace(" kg", "").toDouble()
 
                 if (dateStr.length == 12) {
-                    val parse = LocalDateTime.parse("$dateStr $timeStr", formatter)
-                    val time = Timestamp.valueOf(parse).time
-
-                    map[time] = weight
+                    val dateTime = LocalDateTime.parse("$dateStr $timeStr", formatter)
+                    val toString = dateTime.toString().split("T")[0]
+                    map[toString] = weight
                 } else {
-                    val parse = LocalDateTime.parse("$dateStr $timeStr", formatter2)
-                    val time = Timestamp.valueOf(parse).time
-
-                    map[time] = weight
+                    val dateTime = LocalDateTime.parse("$dateStr $timeStr", formatter2)
+                    val toString = dateTime.toString().split("T")[0]
+                    map[toString] = weight
                 }
             }
         }
@@ -39,6 +39,39 @@ fun mapCSV(fileName: String): MutableMap<Long, Double> {
 
 
 fun main() {
+    val startWeight = 102
+    val goalWeight = 90
+
+
+    val startDate = LocalDate.parse("2021-09-01", DateTimeFormatter.ISO_DATE)
+    val endDate = LocalDate.parse("2022-06-30", DateTimeFormatter.ISO_DATE)
+
+    val days = Duration.between(startDate.atStartOfDay(), endDate.atStartOfDay()).toDays()
+    val total = (startWeight - goalWeight) / days.toDouble()
+
     val mapCSV = mapCSV("C:/Users/kroe/Downloads/Gewicht.csv")
+
+    for (day in 0..days) {
+        val plusDays = startDate.plusDays(day)
+        val weight = startWeight - (day * total)
+        val round = BigDecimal(weight).setScale(2, RoundingMode.HALF_EVEN)
+
+        if (mapCSV[plusDays.toString().split("T")[0]] != null) {
+            val real = mapCSV[plusDays.toString().split("T")[0]]
+            println("$plusDays;$round;$real")
+        } else
+            println("$plusDays;$round;")
+    }
+
+
+    val list = mapCSV.toList().sortedBy { entry -> entry.first }
+
+
+//    list.forEach { entry ->
+//        val dateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(entry.first), TimeZone.getDefault().toZoneId())
+////        println( dateTime.format(formatter))
+//        println(entry.second)
+//    }
+
     println()
 }
--- adventofcode21/src/main/kotlin/day16.kt
+++ adventofcode21/src/main/kotlin/day16.kt
@@ -1,39 +1,35 @@
-import kotlin.math.pow
-
 fun main() {
-    val test = "D2FE28"
-    val test2 = "38006F45291200"
-    val test3 = "EE00D40C823060"
-    val test4 = "8A004A801A8002F478"
-    val test5 = "620080001611562C8802118E34"
 
-
-//    Day16().encode(test)
-//    Day16().encode(test2)
-//    Day16().encode(test3)
-
-//    val d1 = Day16()
-//    d1.encode(test4)
-//    print(d1.sum)
-
-    val d2 = Day16()
-    d2.encode(test5)
-    print(d2.sum)
-
+    val data =
+        "0052E4A00905271049796FB8872A0D25B9FB746893847236200B4F0BCE5194401C9B9E3F9C63992C8931A65A1CCC0D222100511A00BCBA647D98BE29A397005E55064A9DFEEC86600BD002AF2343A91A1CCE773C26600D126B69D15A6793BFCE2775D9E4A9002AB86339B5F9AB411A15CCAF10055B3EFFC00BCCE730112FA6620076268CE5CDA1FCEB69005A3800D24F4DB66E53F074F811802729733E0040E5C5E5C5C8015F9613937B83F23B278724068018014A00588014005519801EC04B220116CC0402000EAEC03519801A402B30801A802138801400170A0046A800C10001AB37FD8EB805D1C266963E95A4D1A5FF9719FEF7FDB4FB2DB29008CD2BAFA3D005CD31EB4EF2EBE4F4235DF78C66009E80293AE9310D3FCBFBCA440144580273BAEE17E55B66508803C2E0087E630F72BCD5E71B32CCFBBE2800017A2C2803D272BCBCD12BD599BC874B939004B5400964AE84A6C1E7538004CD300623AC6C882600E4328F710CC01C82D1B228980292ECD600B48E0526E506F700760CCC468012E68402324F9668028200C41E8A30E00010D8B11E62F98029801AB88039116344340004323EC48873233E72A36402504CB75006EA00084C7B895198001098D91AE2190065933AA6EB41AD0042626A93135681A400804CB54C0318032200E47B8F71C0001098810D61D8002111B228468000E5269324AD1ECF7C519B86309F35A46200A1660A280150968A4CB45365A03F3DDBAE980233407E00A80021719A1B4181006E1547D87C6008E0043337EC434C32BDE487A4AE08800D34BC3DEA974F35C20100BE723F1197F59E662FDB45824AA1D2DDCDFA2D29EBB69005072E5F2EDF3C0B244F30E0600AE00203229D229B342CC007EC95F5D6E200202615D000FB92CE7A7A402354EE0DAC0141007E20C5E87A200F4318EB0C"
+    Day16(data)
 }
 
-class Day16 {
-    var sum = 0
-    fun convertBinaryToDecimal(list: List<Int>): Int {
 
-        var decimalNumber = 0
+class StringTokenizer(val value: String) {
+    private var index = 0
+    val read: Int
+        get() = index
 
-        list.forEachIndexed { i, value ->
-            val test = list.size - i - 1
-            decimalNumber += (value * 2.0.pow(test.toDouble())).toInt()
-        }
+    fun take(amount: Int): String {
+        val s = value.substring(index, index + amount)
+        index += amount
 
-        return decimalNumber
+        return s
+    }
+
+    override fun toString(): String = value.substring(index)
+}
+
+class Day16(input: String) {
+
+    init {
+        var binary = ""
+        input.map { it -> binary += convert(it) }
+        val tree: Packet by lazy { parse(StringTokenizer(binary)) }
+
+        println(tree.versionSum)
+        println(tree.eval())
     }
 
     private fun convert(char: Char): String {
@@ -58,134 +54,76 @@ class Day16 {
         return ""
     }
 
-    private fun convert(stringBit: String): Int {
-        when (stringBit) {
-            "000" -> return 0
-            "001" -> return 1
-            "010" -> return 2
-            "011" -> return 3
-            "100" -> return 4
-            "101" -> return 5
-            "110" -> return 6
-            "111" -> return 7
-        }
-        return -1
-    }
 
-
-    fun toBinary(x: Int, len: Int): String {
-        return String.format(
-            "%" + len + "s",
-            Integer.toBinaryString(x)
-        ).replace(" ".toRegex(), "0")
-    }
-
-    fun encode(data: String) {
-        var bitString = ""
-        data.forEach { char ->
-            bitString += Day16().convert(char)
-        }
-        println(bitString)
-        encode2(bitString.map { s -> s.toString().toInt() }.toList())
-    }
-
-    private fun encode2(list: List<Int>) {
-        if (list.size > 6) {
-            val version = convert("" + list[0] + list[1] + list[2])
-            val id = convert("" + list[3] + list[4] + list[5])
-
-            println("version $version")
-            sum += version
-//        println("id $id")
-
-            if (id == 4) {
-                convertLiteral(list)
+    private fun parse(tok: StringTokenizer, packet: Packet? = null): Packet {
+        val version = tok.take(3).toInt(2)
+        val type = tok.take(3).toInt(2).let { PacketType.values()[it] }
+        var current = packet
+        if (type == PacketType.LIT) {
+            var number = ""
+            do {
+                val v = tok.take(5)
+                number += v.substring(1)
+            } while (v.first() == '1')
+            val lit = Packet(version, type, number.toLong(2))
+            if (current == null) {
+                current = lit
             } else {
-//            val subLists = mutableListOf<List<Int>>()
-                val lengthId = list[6]
+                current.sub += lit
+            }
+        } else {
+            val new = Packet(version, type)
+            if (current != null) {
+                current.sub += new
+            }
+            current = new
 
-                val rest = list.subList(7, list.size)
-                if (lengthId == 1) {
-                    val len = 11
-
-                    val lengthList = rest.subList(0, len)
-                    val listWithoutLength = rest.subList(len, rest.size)
-
-                    val length = convertBinaryToDecimal(lengthList)
-
-                    var i = 0
-                    for (j in 0 until length) {
-                        if (i < listWithoutLength.size - 11) {
-                            val subList = listWithoutLength.subList(i, i + 11)
-                            encode2(subList)
-                        }
-                        i += 11
-                    }
-                    encode2(rest.subList(i, rest.size))
-                } else {
-                    val len = 15
-                    val lengthList = rest.subList(0, len)
-                    val listWithoutLength = rest.subList(len, rest.size)
-
-                    val length = convertBinaryToDecimal(lengthList)
-
-                    val subList1 = listWithoutLength.subList(0, length)
-
-                    var i = 0
-                    for (j in 0 until length) {
-                        if (i > listWithoutLength.size) {
-                            break
-                        }
-                        if (i < subList1.size) {
-                            val lengthId2 = subList1[i]
-                            if (lengthId2 == 0) {
-                                if (i < subList1.size - 15) {
-                                    val subList = subList1.subList(i, i + 16)
-                                    encode2(subList)
-
-                                }
-                                i += 15
-                            } else {
-                                if (i <= subList1.size - 11) {
-                                    val subList = subList1.subList(i, i + 11)
-                                    encode2(subList)
-
-                                }
-                                i += 11
-                            }
-                        }
-                    }
-                    encode2(listWithoutLength.subList(length, listWithoutLength.size))
+            val lenId = tok.take(1).toInt(2)
+            val length = tok.take(if (lenId == 0) 15 else 11).toInt(2)
+            if (lenId == 0) {
+                val readStart = tok.read
+                while (tok.read - readStart < length) {
+                    parse(tok, current)
+                }
+            } else {
+                repeat(length) {
+                    parse(tok, current)
                 }
             }
         }
+
+        return current
     }
 
-    private fun convertLiteral(list: List<Int>) {
-        val version = convert("" + list[0] + list[1] + list[2])
-        val id = convert("" + list[3] + list[4] + list[5])
-//        println("version $version")
-//        println("id $id")
+    data class Packet(
+        val version: Int,
+        val type: PacketType,
+        val value: Long? = null,
+        val sub: MutableList<Packet> = mutableListOf()
+    ) {
+        val versionSum: Int
+            get() = version + sub.sumOf { it.versionSum }
 
-        val subList = list.subList(6, list.size)
-        var i = 0;
-        val till = (subList.size / 5)
-
-        val subLists = mutableListOf<List<Int>>()
-        for (j in 0 until till) {
-            val subList2 = subList.subList(i, i + 5)
-            i += 5
-            subLists.add(subList2)
-            if (subList2[0] == 0) {
-                break
-            }
+        fun eval(): Long = when (type) {
+            PacketType.LIT -> value!!
+            PacketType.SUM -> sub.sumOf { it.eval() }
+            PacketType.PROD -> sub.fold(1L) { acc, n -> acc * n.eval() }
+            PacketType.MIN -> sub.minOf { it.eval() }
+            PacketType.MAX -> sub.maxOf { it.eval() }
+            PacketType.GT -> sub.let { (a, b) -> if (a.eval() > b.eval()) 1 else 0 }
+            PacketType.LT -> sub.let { (a, b) -> if (a.eval() < b.eval()) 1 else 0 }
+            PacketType.EQ -> sub.let { (a, b) -> if (a.eval() == b.eval()) 1 else 0 }
         }
+    }
 
-        val completeLit = mutableListOf<Int>()
-        subLists.forEach { num ->
-            val literal = num.subList(1, num.size)
-            literal.forEach { lit -> completeLit.add(lit) }
-        }
-//        println(convertBinaryToDecimal(completeLit))
+    enum class PacketType {
+        SUM,
+        PROD,
+        MIN,
+        MAX,
+        LIT,
+        GT,
+        LT,
+        EQ
     }
 }
